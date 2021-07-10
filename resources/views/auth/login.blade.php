c7023a59281a34beb639f595d00ccf7e7dd4c009
Marco Musa <marco.musa91@gmail.com>
2021-07-10 19:41:06 +0200

    inizio form login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,20 +4,22 @@
     <div class="row justify-content-center">
         <div class="col-12 col-md-6">
             <form class="shadow px-5 py-5 p-form">
-                <div><h1 class="text-shadow text-white fs-4 text-center text-uppercase mb-3">Login <i class="fas fa-user"></i></h1></div>
+                <div><h1 class="text-shadow text-chromeyellow fs-4 text-center text-uppercase mb-3">Login <i class="fas fa-user"></i></h1></div>
                 <div class="mb-3">
-                  <label for="exampleInputEmail1" class="form-label text-shadow text-white">Email</label>
+                  <label for="exampleInputEmail1" class="form-label text-shadow text-chromeyellow">Email</label>
                   <input type="email" class="form-control rounded-pill" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
                 </div>
                 <div class="mb-3">
-                  <label for="exampleInputPassword1" class="form-label text-shadow text-white">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
+                  <label for="exampleInputPassword1" class="form-label text-shadow text-chromeyellow">Password</label>
+                  <input type="password" class="form-control rounded-pill" id="exampleInputPassword1">
                 </div>
                 <div class="mb-3 form-check">
                   <input type="checkbox" class="form-check-input" id="exampleCheck1">
                   <label class="form-check-label fs-6 text-white" for="exampleCheck1">Ricordami</label>
                 </div>
-                <button type="submit" class="btn btn-lg px-5 rounded-pill text-uppercase shadow py-3 text-white">Login</button>
+                <div class="text-center">
+                <button type="submit" class="btn btn-lg mt-2 px-5 rounded-pill text-uppercase shadow py-3 text-white bg-yellow">Login</button>
+                </div>
               </form>
         </div>
     </div>
