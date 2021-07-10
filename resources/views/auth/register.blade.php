43d19544db4611fb458d939ac99832c9f26bfba6
Marco Musa <marco.musa91@gmail.com>
2021-07-11 00:08:14 +0200

    inizio form registrazione

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,48 @@
+<x-layout>
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6">
+                <form class="shadow px-5 py-5 p-form" method="POST" action="{{route('register')}}">
+                    @csrf
+                    <div><h1 class="text-shadow text-chromeyellow fs-4 text-center text-uppercase mb-3">Registrati <i class="fas fa-address-card"></i></h1></div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label text-shadow text-chromeyellow">Nome e Cognome</label>
+                        <input type="email" class="form-control rounded-pill" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Mario Rossi" name="user">
+                      </div>
+                    <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label text-shadow text-chromeyellow">Email</label>
+                      <input type="email" class="form-control rounded-pill" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="mariorossi@gmail.com" name="email">
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleInputPassword1" class="form-label text-shadow text-chromeyellow">Password</label>
+                      <input type="password" class="form-control rounded-pill" id="exampleInputPassword1" placeholder="123567868" name="password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label text-shadow text-chromeyellow">Conferma Password</label>
+                        <input type="password" class="form-control rounded-pill" id="exampleInputPassword1" placeholder="123567868" name="password">
+                      </div>
+                    <div class="mb-3 form-check">
+                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                      <label class="form-check-label fs-6 text-white" for="exampleCheck1">Ricordami</label>
+                    </div>
+                    <div class="text-center">
+                    <button type="submit" class="btn btn-lg mt-2 px-5 rounded-pill text-uppercase shadow py-3 text-white bg-yellow">Login</button>
+                    </div>
+                    <div class="mt-3 text-center">
+                        <i class="fab fa-google-plus text-danger fs-2"></i>
+                        <i class="fab fa-facebook text-primary fs-2"></i>
+                    </div>
+                  </form>
+            </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    </x-layout>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -20,7 +20,7 @@
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle text-chromeyellow text-shadow" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+              Benvenuto,
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
               <li><a class="dropdown-item" href="#">Logout</a></li>
