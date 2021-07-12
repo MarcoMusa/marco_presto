87d0e931780046c2afb7302480b56db8294cc1a9
Marco Musa <marco.musa91@gmail.com>
2021-07-12 17:25:29 +0200

    relazione user annunci

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Announcement extends Model
 {
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
