5f7d35fb84e7f0b7887e6b4648c317931789bc43
Marco Musa <marco.musa91@gmail.com>
2021-07-14 14:56:07 +0200

    custom lenguages

---
+++ resources/lang/en/it/ui.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'presto' => 'presto',
+    'annunci' => 'announcements'
+
+];
---
+++ resources/lang/en/ui.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'presto' => 'soon',
+    'annunci' => 'announcements',
+    'tutte le categorie' => 'all the categories',
+    'inserisci annuncio' => 'post ad',
+
+];
