fc45c66d026ead094f19fa5afdc8f1b0cf2d5cb4
Marco Musa <marco.musa91@gmail.com>
2021-07-15 12:46:46 +0200

    fine user story 3

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -12,7 +12,10 @@ class PublicController extends Controller
     // VISUALIZZAZIONE ULTIMI 5 ARTICOLI INSERITI-> data di creazione in modo discendente
     public function home()
     {
-        $announcements = Announcement::orderBy('created_at', 'desc')->take(5)->get();
+        $announcements = Announcement::where('is_accepted', true)
+            ->orderBy('created_at', 'desc')
+            ->take(5)
+            ->get();
         return view('homepage', compact('announcements'));
     }
 
@@ -20,7 +23,11 @@ class PublicController extends Controller
     public function announcementsByCategory($name, $category_id)
     {
         $category = Category::find($category_id);
-        $announcements = $category->announcements()->paginate(5);
+        $announcements = $category
+            ->announcements()
+            ->where('is_accepted', true)
+            ->orderBy('created_at', 'desc')
+            ->paginate(5);
         return view('announcements.announcements', compact('category', 'announcements'));
     }
 
