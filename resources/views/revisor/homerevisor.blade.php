c4a1aa80c0625af701ab2c1a37d19a758f5829ac
Marco Musa <marco.musa91@gmail.com>
2021-07-15 11:28:08 +0200

    custom home revisor

--- resources/views/revisor/homerevisor.blade.php
+++ resources/views/revisor/homerevisor.blade.php
@@ -1,18 +1,30 @@
 <x-layout>
 
-<div class="container mg-top">
+<div class="container-fluid shadow bg-white mg-top">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-uppercase text-blu text-center">dashboard di {{Auth::user()->name}}</h1>
+            <div class="row">
+                <span class="badge bg-blu text-chromeyellow  fs-3 fw-bolder justify-content-center">{{\App\Models\Announcement::ToBeRevisionedCount()}}</span>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-12">
-            <div class="card">
-                <div class="card-header">Annuncio # {{$announcement->id}}</div>
+            <div class="card shadow">
+                <div class="card-header bg-yellow">Annuncio {{$announcement->id}}</div>
 
                 <div class="card-body">
 
                     <div class="row">
-                        <div class="col-md-2"><h3>Utente</h3></div>
+                        <div class="col-md-2"><h3 class="text-blu fw-bold">Utente</h3></div>
                         <div class="col-md-10">
-                            # {{$announcement->user->id}},
-                            {{$announcement->user->name}},
+                            {{$announcement->user->id}}, -
+                            {{$announcement->user->name}}, -
                             {{$announcement->user->email}},
                         </div>
                     </div>
